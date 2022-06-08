f550f14294332c2b07f556a85a2ba2f100827841
Danila <jeny2802@yandex.ru>
2022-06-08 13:18:46 +0300

    Изменил запрос
     - Изменил типы данных таблицы в запросе

--- SQLQuery_Create.sql
+++ SQLQuery_Create.sql
@@ -12,16 +12,16 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[Test_Table](
-	[Vremya] [time](7) NOT NULL,
+	[Vremya] [time](0) NOT NULL,
 	[Ms] [smallint] NOT NULL,
 	[Kontroller] [tinyint] NOT NULL,
-	[Sila_tyagi] [numeric](6, 3) NOT NULL,
-	[Napryajenie] [numeric](10, 6) NOT NULL,
-	[Tok_generatora] [numeric](10, 7) NOT NULL,
-	[Tor_kompressora] [numeric](10, 9) NOT NULL,
+	[Sila_tyagi] [decimal](6, 3) NOT NULL,
+	[Napryajenie] [decimal](10, 6) NOT NULL,
+	[Tok_generatora] [decimal](10, 7) NOT NULL,
+	[Tor_kompressora] [decimal](10, 9) NOT NULL,
 	[Temp_gazov_B1] [smallint] NOT NULL,
 	[Temp_gazov_A5] [smallint] NOT NULL,
-	[Tok_ADT] [numeric](10, 9) NOT NULL,
+	[Tok_ADT] [decimal](10, 9) NOT NULL,
 	[Vyhod_TNVD] [smallint] NOT NULL,
 	[Chastota_vrascheniya] [smallint] NOT NULL
 ) ON [PRIMARY]

78827cc745945bd73d400880bc5193e492fb00e6
Danila <jeny2802@yandex.ru>
2022-06-14 16:43:40 +0300

    Разработка приложения ver.0.1

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public IActionResult Import()
+        {
+            return View();
+        }
+
+        public IActionResult DB()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

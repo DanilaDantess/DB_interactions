780e656c8bcda629139399d51e506a1e2e8a6624
Danila <jeny2802@yandex.ru>
2022-06-15 15:52:27 +0300

    Merge branch 'branch2' of https://github.com/DanilaDantess/DB_interactions into branch2

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -23,16 +23,6 @@ namespace WebApp.Controllers
             return View();
         }
 
-        public IActionResult Import()
-        {
-            return View();
-        }
-
-        public IActionResult DB()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

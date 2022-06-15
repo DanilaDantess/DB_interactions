de1ea6847b6a2d4deddc49262c514c2e7b86cfa0
Danila <jeny2802@yandex.ru>
2022-06-15 15:32:50 +0300

    Разработка приложения v0.2
     - Добавлено три представления: Подключение, импорт файла и взаимодействие с БД

---
+++ WebApp/WebApp/Controllers/DatabaseController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public DatabaseController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Connection()
+        {
+            return View();
+        }
+
+        public IActionResult Import()
+        {
+            return View();
+        }
+
+        public IActionResult Extractions()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
